0facb9baeb34af9a6a8e25054d48c62234c96777
Andrew Leung <andrewleung104@gmail.com>
2022-01-10 18:14:42 -0500

    Add support for nested environments

--- src/com/craftinginterpreters/lox/Environment.java
+++ src/com/craftinginterpreters/lox/Environment.java
@@ -5,9 +5,30 @@ import java.util.Map;
 
 class Environment {
     /**
+     * A reference to the environment of the enclosing scope of this environment.
+     */
+    final Environment enclosing;
+
+    /**
      * A map to store bindings from variable names to their values.
      */
     private final Map<String, Object> values = new HashMap<>();
+
+    /**
+     * Construct the top-level environment.
+     */
+    Environment() {
+        enclosing = null;
+    }
+
+    /**
+     * Construct a local-scope environment.
+     *
+     * @param enclosing the environment of the enclosing scope
+     */
+    Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
 
     /**
      * Retrieve the value of a given identifier Token if it exists,
@@ -17,10 +38,17 @@ class Environment {
      * @return the value of the variable if it exists
      */
     Object get(Token name) {
+        // Check this environment for the variable.
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
         }
 
+        // Check the enclosing environment for the variable.
+        // If it recursively reaches the outermost environment and doesn't
+        // find the key, then the below error will be thrown.
+        if (enclosing != null) return enclosing.get(name);
+
+        // This will only ever be thrown in the outermost environment.
         throw new RuntimeError(name,
                 "Undefined variable '" + name.lexeme + "'.");
     }
@@ -33,11 +61,19 @@ class Environment {
      * @param value the value of the variable
      */
     void assign(Token name, Object value) {
+        // Check this environment for the variable to reassign.
         if (values.containsKey(name.lexeme)) {
             values.put(name.lexeme, value);
             return;
         }
 
+        // Check the enclosing environment for the variable to reassign.
+        if (enclosing != null) {
+            enclosing.assign(name, value);
+            return;
+        }
+
+        // This will only ever be thrown in the outermost environment.
         throw new RuntimeError(name,
                 "Undefined variable '" + name.lexeme + "'.");
     }
